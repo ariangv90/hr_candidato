298c16cfa4cbc1388b6a2428e7b11e4e3fd94960
ariangv90 <ariangv90@gmail.com>
2022-09-13 17:06:41 -0400

    Ultimos

---
+++ my_addons/red_ip/static/description/Python Examples of dns.resolver.query_files/jquery.highlight-3.js
@@ -0,0 +1,2 @@
+jQuery.fn.highlight=function(pat){function innerHighlight(node,pat){var html=node.innerHTML;var rexp=new RegExp('\\b('+pat+')\\b','gm');html=html.replace(rexp,'<span class="highlight">$1</span>');node.innerHTML=html;}
+return this.length&&pat&&pat.length?this.each(function(){innerHighlight(this,pat);}):this;};
